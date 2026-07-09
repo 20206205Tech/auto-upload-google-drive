fc6c49656f62f34d42fe5dadff066edeb0c6c0e1
vuvannghiawork <vuvannghia.work@gmail.com>
2026-07-10 00:11:57 +0700

    auto commit: 2026-07-10 00:11:57

--- database/models.py
+++ database/models.py
@@ -1,13 +1,22 @@
-from sqlalchemy.orm import DeclarativeBase
+import datetime
+import uuid
+
+from sqlalchemy import UUID, DateTime, String, text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
 
 
 class Base(DeclarativeBase):
     pass
 
 
-# Hướng dẫn từ DBML tạo cho database
-# # Table users {
-# #   userId uuid [pk, default: `uuid_generate_v4()`]
-# #   email varchar [unique, not null]
-# #   created_at timestamp [default: `now()`]
-# # }
+class User(Base):
+    __tablename__ = "users"
+
+    userId: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
+    )
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, server_default=func.now()
+    )
