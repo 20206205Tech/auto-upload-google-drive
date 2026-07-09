f7d236977ca69174a9c9cd9036e7c788339abdee
vuvannghiawork <vuvannghia.work@gmail.com>
2026-07-10 00:22:23 +0700

    fix migrations

--- database/models.py
+++ database/models.py
@@ -24,7 +24,7 @@ class User(Base):
     __tablename__ = "users"
 
     userId: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
+        UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()")
     )
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     created_at: Mapped[datetime.datetime] = mapped_column(
@@ -44,7 +44,7 @@ class GoogleAccount(Base):
     )
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
+        UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()")
     )
     userId: Mapped[uuid.UUID] = mapped_column(
         ForeignKey("users.userId"), nullable=False
@@ -71,7 +71,7 @@ class TransferJob(Base):
     __tablename__ = "transfer_jobs"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
+        UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()")
     )
     # Tham chiếu trực tiếp đến cột email của bảng users như thiết kế DBML
     source_gmail: Mapped[str] = mapped_column(ForeignKey("users.email"), nullable=False)
